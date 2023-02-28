cf6c81adb3e9b87a51494fca5c3a0e47a5fd7614
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-02-28 18:16:07 +0100

    novità

--- database/migrations/2023_02_28_155657_create_comics_table.php
+++ database/migrations/2023_02_28_155657_create_comics_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50);
+            $table->string('title', 100);
             $table->text('description')->nullable();
             $table->text('thumb')->nullable();
             $table->float('price', 4, 2);
